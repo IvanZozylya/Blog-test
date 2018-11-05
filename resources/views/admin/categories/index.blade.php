81da3251695ae0735b5ad06b6d6be53ec958a535
Ivan <zozylya.ivan@gmail.com>
2018-11-05 18:42:43 +0300

    Create category.delete page

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -24,8 +24,16 @@
                 <tr>
                     <td>{{$category->title}}</td>
                     <td>{{$category->published}}</td>
-                    <td>
-                        <a href="{{route('admin.category.edit', $category)}}"><i class="fa fa-edit"></i></a>
+                    <td class="text-right">
+                        <form onsubmit="if(confirm('Удалить?')){ return true }else{  return false }" method="post"
+                              action="{{route('admin.category.destroy', $category)}}">
+                            <input type="hidden" name="_method" value="DELETE">
+                            {{ csrf_field() }}
+
+                            <a href="{{route('admin.category.edit', $category)}}" class="btn btn-default"><i class="fa fa-edit"></i></a>
+
+                            <button type="submit" class="btn"><i class="fa fa-trash-o"></i></button>
+                        </form>
                     </td>
                 </tr>
             @empty
