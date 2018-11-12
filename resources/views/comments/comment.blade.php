8064e720b539bd081ae2b73a3100907199884948
Ivan <zozylya.ivan@gmail.com>
2018-11-12 02:57:43 +0200

    update comment block (add :Admin)

--- resources/views/comments/comment.blade.php
+++ resources/views/comments/comment.blade.php
@@ -1,38 +1,44 @@
 @foreach($items as $item)
 
-<li id="li-comment-{{$item->id}}" class="comment">
-	<div id="comment-{{$item->id}}" class="comment-container">
-		<div class="comment-author vcard">
-			<img alt="" src="/images/uploads/avatars/{{ $item->user['avatar'] }}" class="avatar" height="75" width="75" />
-			<cite class="fn">{{$item->user['name']}}</cite>
-		</div>
+    <li id="li-comment-{{$item->id}}" class="comment">
+        <div id="comment-{{$item->id}}" class="comment-container">
+            <div class="comment-author vcard">
+                <img alt="" src="/images/uploads/avatars/{{ $item->user['avatar'] }}" class="avatar" height="75"
+                     width="75"/>
+                <cite class="fn">{{$item->user['name']}} @if($item->user['is_admin'] === 1)
+                        {{'(Admin)'}}
+                    @endif
+                </cite>
 
-		<div class="comment-meta commentmetadata">
-			<div class="intro">
-				<div class="commentDate">
-					{{ is_object($item->created_at) ? $item->created_at->format('d.m.Y в H:i') : ''}}                     
-				</div>
+            </div>
 
-			</div>
-			<div class="comment-body">
-				<p>{{ $item->text }}</p>
-			</div>
+            <div class="comment-meta commentmetadata">
+                <div class="intro">
+                    <div class="commentDate">
+                        {{ is_object($item->created_at) ? $item->created_at->format('d.m.Y в H:i') : ''}}
+                    </div>
 
-			@if(Auth::user())
-			<div class="reply group">
-				<a class="comment-reply-link" href="#respond" onclick="return addComment.moveForm(&quot;comment-{{$item->id}}&quot;, &quot;{{$item->id}}&quot;, &quot;respond&quot;, &quot;{{$item->article_id}}&quot;)">Ответить</a>
-			</div>
-			@endif
-		</div>
-	</div>
-	
-	
-	@if(isset($com[$item->id]))
-	<ul class="children">
-		@include('comments.comment', ['items' => $com[$item->id]])
-	</ul>
-	@endif
-	
-</li>
+                </div>
+                <div class="comment-body">
+                    <p>{{ $item->text }}</p>
+                </div>
+
+                @if(Auth::user())
+                    <div class="reply group">
+                        <a class="comment-reply-link" href="#respond"
+                           onclick="return addComment.moveForm(&quot;comment-{{$item->id}}&quot;, &quot;{{$item->id}}&quot;, &quot;respond&quot;, &quot;{{$item->article_id}}&quot;)">Ответить</a>
+                    </div>
+                @endif
+            </div>
+        </div>
+
+
+        @if(isset($com[$item->id]))
+            <ul class="children">
+                @include('comments.comment', ['items' => $com[$item->id]])
+            </ul>
+        @endif
+
+    </li>
 
 @endforeach
