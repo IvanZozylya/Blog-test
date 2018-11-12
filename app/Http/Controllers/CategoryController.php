3e785f8a89d96aba58945db3e1aa7016cc6c0ade
Ivan <zozylya.ivan@gmail.com>
2018-11-12 07:31:50 +0200

    add article span = autor and created_at

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Article;
 use App\Category;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -13,19 +14,21 @@ class CategoryController extends Controller
     public function index()
     {
         return view('categories.index', [
-            'categories' => Category::where('published', 1)->orderBy('created_at','desc')->paginate(6)
+            'categories' => Category::where('published', 1)->orderBy('created_at', 'desc')->paginate(6)
         ]);
     }
 
     //show simple category
     public function show($slug)
     {
+        $users = User::all();
         $category = Category::where('slug', $slug)->first();
-        $articles = Article::where('category_id',$category->id)->where('published', 1)->orderBy('id','desc')->paginate(5);
+        $articles = Article::where('category_id', $category->id)->where('published', 1)->orderBy('id', 'desc')->paginate(5);
 
         return view('categories.show', [
             'category' => $category,
             'articles' => $articles,
+            'users' => $users
         ]);
     }
 }
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -6,17 +6,25 @@
     <div class="container">
         @if(Auth::user())
             <div class="row clearfix margin-bottom">
-                <a class="btn btn-success pull-right " href="{{route('create_article')}}" style="color:black">Create article</a>
+                <a class="btn btn-success pull-right " href="{{route('create_article')}}" style="color:black">Create
+                    article</a>
             </div>
         @endif
         @forelse($articles as $article)
             <div class="row article-block" onclick="window.location='{{ route('show_article', $article->slug) }}'">
                 <div class="col-md-2">
-                    <img style="width:100px; height:100px; float:left; border-radius:50%; margin-right:25px;" src="{{asset('/images/uploads/articles/' . $article->image)}}">
+                    <img style="width:100px; height:100px; float:left; border-radius:50%; margin-right:25px;"
+                         src="{{asset('/images/uploads/articles/' . $article->image)}}">
                 </div>
                 <div class="col-sm-10">
                     <span class="article-title">{{$article->title}}</span>
                     <span class="article-description">{!!$article->description_short!!}</span>
+                    <span style="display:block;">Опубликовано: {{$article->created_at}}</span>
+                    @foreach($users as $user)
+                        @if($user->id == $article->created_by)
+                            <span style="display:block;">Автор: {{$user->name}}</span>
+                        @endif
+                    @endforeach
                 </div>
             </div>
         @empty
